086cad7a60c282ead0c443642ea3bb99e9063496
Juan-cruz22 <ghigliani02@gmail.com>
2025-07-23 10:08:57 -0300

    solucionandoVercel404

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-import ContactContextProvider from './Context/ContactContext.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-        <ContactContextProvider>
             <App />
-        </ContactContextProvider>
     </BrowserRouter>
 )
